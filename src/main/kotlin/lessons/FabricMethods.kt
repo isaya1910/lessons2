a727834d0ecc43f89e3e5e279cfa86cfe4482373
isaya1910 <ibaigereyev@klika-tech.com>
2021-06-25 00:56:50 +0600

    Comments

--- src/main/kotlin/lessons/FabricMethods.kt
+++ src/main/kotlin/lessons/FabricMethods.kt
@@ -3,11 +3,14 @@ package lessons
 
 class Complex private constructor(val data: Int) {
     companion object {
+
+        // this fabric method need to use only with positive values
         fun complexFromPositiveValue(data: Int): Complex {
             require(data > 0)
             return Complex(data)
         }
 
+        // this fabric method need to use only with negative values
         fun complexFromNegativeValue(data: Int): Complex {
             require(data < 0)
             return Complex((data))
