ec7f4f9ef924d544cb6950ec51380cde68faeed4
isaya1910 <isaya1910@gmail.com>
2022-08-18 01:37:30 +0600

    Update BoundedStack.kt

--- src/main/kotlin/lessons/BoundedStack.kt
+++ src/main/kotlin/lessons/BoundedStack.kt
@@ -1,4 +1,5 @@
-abstract class BoundedStack<T>(val maxElementsCount: Int = 32) {
+// constructor with maxSize of stack and default value is 32
+class BoundedStack<T>(private val maxSize: Int = 32) {
     companion object {
         const val POP_NIL = 0
         const val POP_OK = 1
@@ -10,30 +11,36 @@ abstract class BoundedStack<T>(val maxElementsCount: Int = 32) {
         const val PUSH_OK = 1
         const val PUSH_ERR = 2
     }
- 
-    private val stack = mutableListOf<T>()
-    private var popStatus = POP_NIL
-    private var peekStatus = PEEK_NIL
+
+    private val stack: MutableList<T> = mutableListOf()
+    private var peekStatus = POP_NIL
+    private var popStatus = PEEK_NIL
     private var pushStatus = PUSH_NIL
- 
-    fun push(element: T) {
-        if (stack.size < maxElementsCount) {
-            stack.add(element)
+
+    // precondition: stack size is less than maxSize
+    // postcondition: item was added
+    fun push(item: T) {
+        if (stack.size < maxSize) {
             pushStatus = PUSH_OK
+            stack.add(item)
             return
         }
         pushStatus = PUSH_ERR
     }
- 
+
+    // precondition: stack is not empty
+    // postcondition: last item is deleted
     fun pop() {
         if (stack.isNotEmpty()) {
-            stack.removeLast()
+            val itemToDelete = stack.last()
+            stack.remove(itemToDelete)
             popStatus = POP_OK
             return
         }
         popStatus = POP_ERR
     }
- 
+
+    // precondition: stack is not empty
     fun peek(): T? {
         if (stack.isNotEmpty()) {
             peekStatus = PEEK_OK
@@ -42,21 +49,29 @@ abstract class BoundedStack<T>(val maxElementsCount: Int = 32) {
         peekStatus = PEEK_ERR
         return null
     }
- 
+
     fun size(): Int {
         return stack.size
     }
- 
+
+    // postcondition: all items are removed from stack and statuses are set for initial values
     fun clear() {
         stack.clear()
-        popStatus = POP_NIL
         peekStatus = PEEK_NIL
-        pushStatus = PUSH_NIL
+        popStatus = POP_NIL
+        pushStatus = POP_NIL
     }
- 
-    fun getPeekStatus() = peekStatus
- 
-    fun getPopStatus() = popStatus
- 
-    fun getPushStatus() = pushStatus
+
+    // Statuses request
+    fun getPushStatus(): Int {
+        return pushStatus
+    }
+
+    fun getPopStatus(): Int {
+        return popStatus
+    }
+
+    fun getPeekStatus(): Int {
+        return peekStatus
+    }
 }
