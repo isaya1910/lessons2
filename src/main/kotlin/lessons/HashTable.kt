29372598c8e1ce1b72025a134d07893c8db450c9
isaya1910 <isaya1910@gmail.com>
2022-08-23 20:56:08 +0600

    Create HashTable.kt

---
+++ src/main/kotlin/lessons/HashTable.kt
@@ -0,0 +1,15 @@
+
+abstract class HashTable <T> {
+    // postcondition: new value was inserted
+    abstract fun insert(value: T)
+
+    // precondition: value exist in hash tavle
+    // postcondition: value was deleleted
+    abstract fun remove(value: T)
+
+    // return true if value exist
+    abstract fun find(value: T): Boolean
+
+    abstract fun getInsertStatus()
+    abstract fun getDeleteStatus()
+}
