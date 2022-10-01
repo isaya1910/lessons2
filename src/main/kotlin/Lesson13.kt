0531962ce30fcee2167eef06315ce014cb0dba7a
isaya1910 <isaya1910@gmail.com>
2022-10-01 20:03:39 +0600

    Update Lesson13.kt

--- src/main/kotlin/Lesson13.kt
+++ src/main/kotlin/Lesson13.kt
@@ -4,3 +4,7 @@ open class Aircraft(val model: String)
 class MilitaryAircraft(model: String) : Aircraft(model)
 
 class CivilAircraft(model: String) : Aircraft(model)
+
+/*
+* class Aircraft(val model: String, val type: Int) // 0 MilitaryAircraft 1 CivilAircraft
+/
