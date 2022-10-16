908fffb296008289a3dec5d47aaf974e4a2ac7ae
isaya1910 <isaya1910@gmail.com>
2022-10-17 00:43:32 +0600

    Update Lesson20.kt

--- src/main/kotlin/Lesson20.kt
+++ src/main/kotlin/Lesson20.kt
@@ -2,3 +2,27 @@
   Класс машины и класс двигатель. 
   У машины можеть быть разные потомки, например седаны или внедорожники и тд, у каждого из них могут быть свои двигатели со своими разичными видами
 */
+
+interface Car {
+    fun getEngine(): Engine
+}
+
+@JvmInline
+value class HorsePower(val value: Int)
+
+interface Engine {
+    fun getPower(): HorsePower
+}
+
+object ElectricEngine: Engine {
+    override fun getPower(): HorsePower {
+        return HorsePower(1000)
+    }
+
+}
+
+class ElectricCar: Car {
+    override fun getEngine(): Engine {
+        return ElectricEngine
+    }
+}
